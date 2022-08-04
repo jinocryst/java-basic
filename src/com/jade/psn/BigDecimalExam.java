64c638a469e92b7dc4d36fffb69657f44cd030c9
parkforte <parkforte@naver.com>
2022-08-04 14:19:26 +0900

    commit

---
+++ src/com/jade/psn/BigDecimalExam.java
@@ -0,0 +1,8 @@
+package com.jade.psn;
+
+public class BigDecimalExam {
+    public static void main(String[] args) {
+        double bigDecimal = 2.209000001154468e+21;
+        System.out.println(bigDecimal);
+    }
+}
