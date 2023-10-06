8495dbff09bbd815f652f27c43b55e4cfd0f0a94
Jade Park <dev.jadepark@gmail.com>
2023-10-06 09:56:01 +0900

    쓰레드 예제

---
+++ src/dev/jadepark/personal/StringEqualsTest.java
@@ -0,0 +1,11 @@
+package dev.jadepark.personal;
+
+public class StringEqualsTest {
+    public static void main(String[] args) {
+        String a = "healthnyou-SS001";
+        String b = "healthnyou-SS001";
+
+        System.out.println(a.equals(b));
+        System.out.println(b.equals(a));
+    }
+}
