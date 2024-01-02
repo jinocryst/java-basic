541b01eb409e872fdf4bb8fbac15a9afe79e0425
Jade Park <dev.jadepark@gmail.com>
2024-01-02 11:14:27 +0900

    네트워크 예제1

---
+++ src/dev/jadepark/standard/ch16networking/NetworkEx3.java
@@ -0,0 +1,16 @@
+package dev.jadepark.standard.ch16networking;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class NetworkEx3 {
+    public static void main(String[] args) throws IOException {
+
+        URL url = new URL("http://healthnyou-dev.thejoin.co.kr");
+        URLConnection conn = url.openConnection();
+        System.out.println("conn : " + conn.toString());
+        System.out.println("contentType : " + conn.getContentType());
+    }
+}
